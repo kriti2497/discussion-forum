66663f5413d43dffaaa2d9305f264a1b5b516168
Kritika Bandekar <kriti2497@gmail.com>
2024-07-02 09:09:31 +0530

    feat: added comment section listing, with nested comments

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,5 +1,8 @@
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import CommentList from "@/components/comments/comment-list";
 import Link from "next/link";
 import PostShow from "@/components/posts/post-show";
+import { fetchCommentsByPostId } from "@/db/queries/comments";
 import paths from "@/paths";
 
 interface PostShowPageProps {
@@ -10,12 +13,15 @@ interface PostShowPageProps {
 }
 const ViewPostPage = ({ params }: PostShowPageProps) => {
   const { slug, postId } = params;
+
   return (
     <div className="space-y-3">
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {`< Back to ${slug}`}
       </Link>
       <PostShow postId={postId} />
+      <CommentCreateForm postId={postId} startOpen />
+      <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
     </div>
   );
 };
